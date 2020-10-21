81241cffae6b166cf71ccd357bcc005fcd65f2e0
marios-dev <73258210+marios-dev@users.noreply.github.com>
2020-10-22 01:28:00 +0300

    Add files via upload

---
+++ Rainbow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainbow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> rainbow = new List<string>();
+            rainbow.Add("red");
+            rainbow.Add("yellow");
+            rainbow.Add("orange");
+            rainbow.Add("green");
+            rainbow.Add("blue");
+            rainbow.Add("indigo");
+            rainbow.Add("violet");
+
+            for (int i = 0; i <= rainbow.Count; i++)
+            {
+                Console.WriteLine(rainbow[i]);
+            }
+            // rainbow.Reverse();
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
